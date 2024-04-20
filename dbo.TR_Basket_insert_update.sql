26c2b014a3b6cf9effbe898c2005186882f339b5
JuliaSokolnikova <166937312+JuliaSokolnikova@users.noreply.github.com>
2024-04-20 18:13:52 +0300

    Add files via upload

---
+++ dbo.TR_Basket_insert_update.sql
@@ -0,0 +1,26 @@
+﻿create or alter trigger dbo.tr_Basket_insert_update
+on dbo.Basket
+after insert
+as
+begin
+	drop table if exists #InsetredSKU
+	select
+		count(i.ID_SKU) as cnt
+		,i.ID_SKU
+	into #InsertedSKU
+	from inserted as i
+	group by i.ID_SKU
+
+	if 2 <= any (select ins.cnt from #InsertedSKU as ins)
+		update b
+		set DiscountValue = b.Value * 0.05
+		from dbo.Basket as b
+		where b.ID_SKU in (select ins.ID_SKU from #InsertedSKU as ins where ins.cnt >= 2)
+			and b.ID in (select i.ID from inserted as i)
+	else
+		update b
+		set DiscountValue = 0
+		from dbo.Basket as b
+		where b.ID_SKU in (select ins.ID_SKU from #InsertedSKU as ins where ins.cnt < 2)
+			and b.ID in (select i.ID from inserted as i)
+end
